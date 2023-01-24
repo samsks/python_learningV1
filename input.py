04b6a80bc023bb96dcbbc423fa2469ba61621e30
SamSKS <samuel.meireles.15s@outlook.com>
2023-01-23 22:08:38 -0300

    add: input file

---
+++ input.py
@@ -0,0 +1,10 @@
+# input.py
+nome_do_pet = input("Digite o nome do seu pet: ")
+print(f"O nome do seu pet é {nome_do_pet}")
+
+
+# input sempre lidos como string. Se precisar trabalhar com outros
+# tipos, converter
+
+idade = int(input("Digite sua idade: "))
+altura = float(input("Digite sua altura em metros: "))
